5bc3b2db49dbbb7f8037ef52514a505770648f66
yejincode <syjsyj2579@naver.com>
2021-10-24 02:48:25 +0900

    add bottomtab

--- app/src/main/java/com/example/petviews/MainActivity.java
+++ app/src/main/java/com/example/petviews/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout layout_interest;
     LinearLayout layout_livevideo;
     LinearLayout layout_highlight;
+    ImageButton tab_home;
+    ImageButton tab_photo;
+    ImageButton tab_camera;
+    ImageButton tab_profile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +77,52 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        // 하단 탭
+
+        // 하단 탭 홈버튼
+        tab_home = findViewById(R.id.tab_home);
+        tab_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),MainActivity.class);
+                startActivity(intent); //홈에서 홈..? 이렇게 이어주어야 할까..?
+            }
+        });
+
+        // 하단 탭 사진버튼
+        tab_photo = findViewById(R.id.tab_photo);
+        tab_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { //액티비티 무엇일지,,,,,,,,,
+                Intent intent = new Intent(view.getContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 하단 탭 카메라버튼
+        tab_camera = findViewById(R.id.tab_camera);
+        tab_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { //카메라액티비티로 이어지는게 맞는지,,,
+                Intent intent = new Intent(view.getContext(),cameraActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 하단 탭 프로필버튼
+        tab_profile = findViewById(R.id.tab_profile);
+        tab_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { //이건 또 어디로 이어지는건지,,,,
+                Intent intent = new Intent(view.getContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 
 
